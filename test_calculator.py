e61b9b413737660514886e286b285a9271a6cd8e
Sujeeth Shetty <sujeeth.shetty@icloud.com>
2023-05-23 22:42:04 -0400

    Circle CI Demo

---
+++ test_calculator.py
@@ -0,0 +1,17 @@
+#create pytest file for testing calculator.py
+import calculator
+
+class TestCalculator:
+    
+    def test_addition(self):
+        assert 4 == calculator.add(2,2)
+
+    def test_subtraction(self):
+        assert 2 == calculator.subtract(4,2)
+
+    def test_multiplication(self):
+        assert 100 == calculator.multiply(10,10)
+
+    def test_division(self):
+        assert 10 == calculator.divide(100,10)
+
