67459144462d94240e1fa3a9d50dc286e4cf2d9b
Sujeeth Shetty <sujeeth.shetty@icloud.com>
2023-05-24 21:37:01 -0400

    config changes

--- src/test_calculator.py
+++ src/test_calculator.py
@@ -15,3 +15,5 @@ class TestCalculator:
     def test_division(self):
         assert 10 == calculator.divide(100,10)
 
+    def test_division(self):
+        assert 100 == calculator.divide(1000,10)
